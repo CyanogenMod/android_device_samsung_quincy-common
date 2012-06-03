32235f9bc115194350aa769d2170f9a244de4065
Steve Kondik <shade@chemlab.org>
2012-06-03 01:17:39 -0700

    quincy: Initial commit of common Quincy device
     * Splitting off from quincyatt for future proofing.

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,63 @@
+# Copyright (C) 2009 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+# inherit from common msm8660
+-include device/samsung/msm8660-common/BoardConfigCommon.mk
+
+# Kernel
+BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom usb_id_pin_rework=true
+BOARD_KERNEL_BASE           := 0x48000000
+BOARD_FORCE_RAMDISK_ADDRESS := 0x49400000
+BOARD_KERNEL_PAGESIZE       := 2048
+
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk0p28
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+
+# FIXME: Overlay has an issue on Quincy when playing video in landscape mode
+COMMON_GLOBAL_CFLAGS += -DQCOM_ROTATOR_KERNEL_FORMATS
+
+# Workaround for glitches while cropping bypass layers
+TARGET_NO_BYPASS_CROPPING := true
+
+# MTP
+BOARD_MTP_DEVICE := "/dev/usb_mtp_gadget"
+
+# Audio
+TARGET_USES_QCOM_LPA := true
+COMMON_GLOBAL_CFLAGS += -DWITH_QCOM_LPA
+
+# Disable MVS audio, it causes robot voice in GTalk
+COMMON_GLOBAL_CFLAGS += -DNO_QCOM_MVS
+
+# Disable initlogo, Samsungs framebuffer is weird
+TARGET_NO_INITLOGO := true
+
---
+++ quincy-common.mk
@@ -0,0 +1,58 @@
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+## The gps config appropriate for this device
+PRODUCT_COPY_FILES += device/common/gps/gps.conf_US_SUPL:system/etc/gps.conf
+
+## (2) Also get non-open-source specific aspects if available
+$(call inherit-product-if-exists, vendor/samsung/quincy-common/quincy-common-vendor.mk)
+
+## overlays
+DEVICE_PACKAGE_OVERLAYS += device/samsung/quincy-common/overlay
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    device/samsung/quincy-common/ramdisk/init.qcom.rc:root/init.qcom.rc \
+    device/samsung/quincy-common/ramdisk/init.qcom.sh:root/init.qcom.sh \
+    device/samsung/quincy-common/ramdisk/init.qcom.usb.sh:root/init.qcom.usb.sh \
+    device/samsung/quincy-common/ramdisk/init.target.rc:root/init.target.rc \
+    device/samsung/quincy-common/ramdisk/initlogo.rle:root/initlogo.rle \
+    device/samsung/quincy-common/ramdisk/ueventd.rc:root/ueventd.rc \
+	device/samsung/quincy-common/ramdisk/init.emmc.rc:root/init.emmc.rc
+
+# BT firmware
+PRODUCT_COPY_FILES += \
+    device/samsung/quincy-common/firmware/bcm4330B1.hcd:system/etc/firmware/bcm4330B1.hcd
+
+# S-Pen IDC
+PRODUCT_COPY_FILES += \
+    device/samsung/quincy-common/idc/sec_e-pen.idc:system/usr/idc/sec_e-pen.idc
+
+# Vold configuration
+PRODUCT_COPY_FILES += \
+    device/samsung/quincy-common/vold.fstab:system/etc/vold.fstab
+
+# common msm8660
+$(call inherit-product, device/samsung/msm8660-common/msm8660.mk)
+
+$(call inherit-product, frameworks/base/build/phone-xhdpi-1024-dalvik-heap.mk)
+
